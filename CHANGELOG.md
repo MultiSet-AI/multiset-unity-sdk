792c0f21b03de8c4088a64aa39111be74d4371e3
Shyam Barange <developer.shyam11@gmail.com>
2025-07-30 14:06:56 +0530

    Initial release of MultiSet Unity SDK

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+SDK 1.8.0
+
+## [1.8.0] - 2025-30-07
+### Added
+- Initial release of MultiSet SDK
+- Localization and Navigation sample scenes
+- Model Tracking sample scene
+- Training mode scene added
+
---
+++ Runtime/Scripts/Navigation/Utils/AgentPosition.cs
@@ -0,0 +1,50 @@
+/*
+Copyright (c) 2025 MultiSet AI. All rights reserved.
+Licensed under the MultiSet License. You may not use this file except in compliance with the License. and you can’t re-distribute this file without a prior notice
+For license details, visit www.multiset.ai.
+Redistribution in source or binary forms must retain this notice.
+*/
+
+using UnityEngine;
+using UnityEngine.AI;
+
+/**
+ * Keeps NavMeshAgent always on NavMesh where AR camera is currently located.
+ */
+public class AgentPosition : MonoBehaviour
+{
+    // AR camera of the scene
+    GameObject ARcamera;
+
+    // NavMeshAgent component on same GameObject
+    NavMeshAgent agent;
+
+    // mesh of agent
+    MeshRenderer mesh;
+
+    void Awake()
+    {
+        ARcamera = Camera.main.gameObject;
+        agent = GetComponent<NavMeshAgent>();
+        mesh = GetComponent<MeshRenderer>();
+        mesh.enabled = false;
+    }
+
+    void Update()
+    {
+        // teleport agent to camera x & z, but take y from agent object
+        agent.Warp(new Vector3(ARcamera.transform.position.x, agent.gameObject.transform.position.y, ARcamera.transform.position.z));
+
+        // if agent is too far away from camera reset the position of agent, local position of agent is checked, because agent is child of camera
+        if (agent.gameObject.transform.localPosition.y > 0 && agent.gameObject.transform.localPosition.y > 1.5)
+        {
+            // 1.5 is when camera is down but agent on upper floor
+            agent.Warp(new Vector3(ARcamera.transform.position.x, ARcamera.transform.position.y - 1.5f, ARcamera.transform.position.z));
+        }
+        else if (agent.gameObject.transform.localPosition.y < 0 && agent.gameObject.transform.localPosition.y < -3.5)
+        {
+            // -3.5 is when camera is up, but agent is on floor below
+            agent.Warp(new Vector3(ARcamera.transform.position.x, ARcamera.transform.position.y + 3.5f, ARcamera.transform.position.z));
+        }
+    }
+}
